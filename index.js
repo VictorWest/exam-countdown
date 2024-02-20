78b81bfdce77f56e4fb252c3a5ccfdfa79dd29e4
VictorWest <106874037+VictorWest@users.noreply.github.com>
2024-02-20 14:18:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ var x = setInterval(function(){
     var d = deadLine - currentDate;
 
     var days = Math.floor(d / (1000 * 60 * 60 * 24));
-    console.log(days);
     document.querySelector(".firstRow").innerHTML = `${days}`;
     var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    console.log(hours);
     document.querySelector(".secondRow").innerHTML = `${hours}`;
     var minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
-    console.log(minutes);
     document.querySelector(".thirdRow").innerHTML = `${minutes}`;
     var seconds = Math.floor((d % (1000 * 60)) / (1000));
-    console.log(seconds);
     document.querySelector(".fourthRow").innerHTML = `${seconds}`;
 
     if(d < 0){
@@ -28,7 +24,6 @@ var x = setInterval(function(){
         document.querySelector(".fourthRow").innerHTML = `...`;
         for(var i = 0; i < (document.querySelectorAll("p")).length; i++){
             document.querySelectorAll("p")[i].innerHTML = "";
-            console.log((document.querySelectorAll("p")).length);
         }
     }
 }, 1000);
